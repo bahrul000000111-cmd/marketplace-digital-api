357a010f13936dfc3d6b1ecae9e5df03c7011dab
bahrul000000111-cmd <bahrul000000111@gmail.com>
2026-04-09 20:25:50 +0800

    feat: add stock column to products table in migration

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -14,6 +14,6 @@ class ProductCategory extends Model
     // Relasi: satu kategori punya banyak produk
     public function products()
     {
-        return $this->hasMany(Product::class);
+        return $this->hasMany(Product::class, 'category_id');
     }
 }
